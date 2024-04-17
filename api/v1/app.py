8d1be74eb275c43d5407a3a6436a2459650325e9
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-17 14:10:43 +0200

    add error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,6 +17,11 @@ def close_db(error):
     storage.close()
 
 
+@app.errorhandler(404)
+def error_404(error):
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', '5000')
