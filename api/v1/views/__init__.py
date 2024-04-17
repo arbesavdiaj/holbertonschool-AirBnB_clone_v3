ef286293422f042ed4a357d81d9014965bc90da4
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-17 13:51:03 +0200

    add new code

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,4 +5,6 @@ Moudle to create Blueprint for our API
 """
 
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views import *
