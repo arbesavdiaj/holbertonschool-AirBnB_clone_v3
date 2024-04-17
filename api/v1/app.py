6fe34e17557590e7bfb6351ca6a0fa187ab662e5
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-17 11:56:08 +0200

    add all necessary code

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,19 @@
 from flask import Flask
-from models import storage  # Importing storage from models module
-from api.v1.views import app_views  # Importing app_views from views module
+from models import storage
+from api.v1.views import app_views
 
-
-app = Flask(__name__)  # Creating an instance of Flask
+app = Flask(__name__)
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """
-    Method to handle app teardown.
-    """
-    storage.close()  # Closing storage when app context is torn down
+def close_db(error):
+    """Close storage on teardown"""
+    storage.close()
 
 
 if __name__ == "__main__":
+    import os
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)  # Running Flask server
+    port = os.getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status')
 def status():
     """
     Route to return the status.
