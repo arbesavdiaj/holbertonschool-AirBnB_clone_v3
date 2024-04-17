438bd2ea6740f2a18b650f40a9093c28b68074df
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-17 11:50:03 +0200

    add files

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,10 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     """
     Route to return the status.
     """
-    return jsonify({"status": "OK"})  # Returning a JSON response with status OK
+    return jsonify({"status": "OK"})
