fd180dada0037a4c7d77e84e236feefc7e3fbe63
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-17 11:42:25 +0200

    add all neccessary codes and comments

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,36 +4,23 @@ Module to instatiate an flask app
 to deploy our API
 '''
 from flask import Flask
+import os
 from models import storage
-from os import getenv
-from flask import jsonify
 from api.v1.views import app_views
-from flask_cors import CORS
 
 
-app = Flask(__name__)
-CORS(app, origins=['0.0.0.0'])
-app.register_blueprint(app_views)
-
-
-@app.errorhandler(404)
-def error_404(error):
-    return jsonify({"error": "Not found"}), 404
+app = Flask(__name__)  # Creating an instance of Flask
 
 
 @app.teardown_appcontext
-def close_db(error):
-    '''
-    Function call to close db connection
-    after each app teardwon
-    '''
+def teardown_appcontext(exception):
+    """
+    Method to handle app teardown.
+    """
     storage.close()
 
 
 if __name__ == "__main__":
-    '''
-    Starting server
-    '''
-    app.run(host=getenv("HBNB_API_HOST", default="0.0.0.0"),
-            port=int(getenv("HBNB_API_PORT", default=5000)),
-            threaded=True)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,38 +5,11 @@ for our api
 '''
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
-from models.amenity import Amenity
-from models.base_model import BaseModel, Base
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
-
-
-@app_views.route("/status", strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def status():
-    '''
-    Status endpoint
-    '''
+    """
+    Route to return the status.
+    """
     return jsonify({"status": "OK"})
-
-
-@app_views.route("/stats", strict_slashes=False)
-def stats():
-    '''
-    Stats endpoint
-    '''
-    status = {}
-
-    return jsonify({"amenities": storage.count(Amenity),
-                    "cities": storage.count(City),
-                    "places": storage.count(Place),
-                    "reviews": storage.count(Review),
-                    "states": storage.count(State),
-                    "users": storage.count(User)})
