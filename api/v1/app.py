a3f467d1db3c5714afae68d6859d838d4c9498e2
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-17 11:56:51 +0200

    add all necessary code

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -13,7 +14,7 @@ def close_db(error):
 
 
 if __name__ == "__main__":
-    import os
+    """comment"""
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
