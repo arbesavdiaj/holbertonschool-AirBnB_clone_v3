d087364186f4494ddcf9204a362e958d3666c754
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-17 12:21:21 +0200

    add files

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,21 +5,35 @@ to deploy our API
 '''
 from flask import Flask
 from models import storage
+from os import getenv
+from flask import jsonify
 from api.v1.views import app_views
-import os
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app, origins=['0.0.0.0'])
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def error_404(error):
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.teardown_appcontext
 def close_db(error):
-    """Close storage on teardown"""
+    '''
+    Function call to close db connection
+    after each app teardwon
+    '''
     storage.close()
 
 
 if __name__ == "__main__":
-    """comment"""
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port, threaded=True)
+    '''
+    Starting server
+    '''
+    app.run(host=getenv("HBNB_API_HOST", default="0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", default=5000)),
+            threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+from flask import Blueprint
 """
 Moudle to create Blueprint for our API
 """
 
-from flask import Blueprint
-from api.v1.views import index
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+
+from api.v1.views.states import *
+from api.v1.views.index import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
